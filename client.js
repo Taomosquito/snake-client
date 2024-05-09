e7fd0ab611f5c3d42d26123621b12e0a1b3b7316
Taomosquito <S_Peloquin123@live.com>
2024-05-08 21:25:42 -0400

    moved content from 1 file to another to segment code

---
+++ client.js
@@ -0,0 +1,24 @@
+const net = require("net");
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243", // IP address between host: and  //,
+    port: 50541// PORT number between port: and //,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  //This is what a listener looks like when it is lisening for an on connection event.
+  conn.on("connect", () => {
+    // code that does something when the connection is first established
+  });
+
+  //This is what a listener looks like when it is lisening for an on connection event.
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+  return conn;
+};
+
+module.exports = connect;
