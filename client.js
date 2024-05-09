adeda1dd404f494e176842d9626087b3552c0dbb
Taomosquito <S_Peloquin123@live.com>
2024-05-08 22:32:23 -0400

    made code work with name and purged of comments until clearer comments can be added once full functionality is reached!

--- client.js
+++ client.js
@@ -1,24 +1,36 @@
 const net = require("net");
 // establishes a connection with the game server
 const connect = function () {
-  const conn = net.createConnection({
+  const serverConnection = net.createConnection({
     host: "165.227.47.243", // IP address between host: and  //,
     port: 50541// PORT number between port: and //,
   });
 
   // interpret incoming data as text
-  conn.setEncoding("utf8");
+  serverConnection.setEncoding("utf8");
 
   //This is what a listener looks like when it is lisening for an on connection event.
-  conn.on("connect", () => {
-    // code that does something when the connection is first established
+  serverConnection.on("connect", () => {
+    console.log("connection to game server has been established")
+    serverConnection.write("Name: SDP");
   });
 
+
+
+  // // standard version of .on event.
+  //conn.on("connect", functionThatDoesSomething);
+
+  // conn.on("connect", (client) => {
+  //   console.log(client);
+  //   
+  // });
+
   //This is what a listener looks like when it is lisening for an on connection event.
-  conn.on("data", (data) => {
+  serverConnection.on("data", (data) => {
     console.log(data);
   });
-  return conn;
+  return serverConnection;
 };
 
 module.exports = connect;
+
