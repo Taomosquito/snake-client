be3ddaedd6af4e1c77b9fa570ad0f1cf43a57fb8
Taomosquito <S_Peloquin123@live.com>
2024-05-09 15:08:36 -0400

    managed to make the refactor work!

--- play.js
+++ play.js
@@ -1,46 +1,6 @@
+const setupInput = require("./input");
 const connect = require("./client");
 console.log("Connecting ...");
 // connect();
-let serverConnection
-
-const setupInput = function (connection) {
-  const stdin = process.stdin;
-  serverConnection = connection;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  // \u0003 maps to ctrl+c input
-  if (key === "\u0003") {
-    process.exit();
-  }
-  if (key === "w") {
-    //const autoMoveUp = function () {
-    serverConnection.write("Move: up");
-  }
-  // setInterval(autoMoveUp, 50);
-  // }
-  if (key === "s") {
-    // const autoMoveDown = function () {
-    serverConnection.write("Move: down");
-  }
-  //setInterval(autoMoveDown, 50);
-  // }
-  if (key === "a") {
-    // const autoMoveLeft = function () {
-    serverConnection.write("Move: left");
-  }
-  // setInterval(autoMoveLeft, 50);
-  // }
-  if (key === "d") {
-    //const autoMoveRight = function () {
-    serverConnection.write("Move: right");
-  }
-  // setInterval(autoMoveRight, 50);
-  // }
-};
-setupInput(connect())
+console.log("Connecting ...");
+setupInput(connect());
